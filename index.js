5d60c01553c9f145c11b42a9fcd8d778f62381b4
Trevor Blades <tdblades@gmail.com>
2019-07-17 17:40:04 -0700

    Check for wrapping component

--- index.js
+++ index.js
@@ -12,16 +12,29 @@ function insertAt(array, index, item) {
   return [...array.slice(0, index), item, ...array.slice(index)];
 }
 
+function isReactComponent(node, tag) {
+  return node && node.type === 'jsx' && node.value === tag;
+}
+
 module.exports = ({markdownAST}) => {
   visit(markdownAST, 'code', (node, index, parent) => {
     if (/tsx?/.test(node.lang)) {
-      const value = ts.transpile(node.value, compilerOptions).trim();
-      if (value) {
-        parent.children = insertAt(parent.children, index + 1, {
-          type: 'code',
-          lang: /x$/.test(node.lang) ? 'jsx' : 'js',
-          value
-        });
+      const prevNode = parent.children[index - 1];
+      const nextNode = parent.children[index + 1];
+      // TODO: make wrapping component checks opt-in
+      // have the user specify the tagname to look for with options
+      if (
+        isReactComponent(prevNode, '<MultiCodeBlock>') &&
+        isReactComponent(nextNode, '</MultiCodeBlock>')
+      ) {
+        const value = ts.transpile(node.value, compilerOptions).trim();
+        if (value) {
+          parent.children = insertAt(parent.children, index + 1, {
+            type: 'code',
+            lang: /x$/.test(node.lang) ? 'jsx' : 'js',
+            value
+          });
+        }
       }
     }
   });
