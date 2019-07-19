f045bfcaca565a22b20a85f608164f3daaf47846
Trevor Blades <tdblades@gmail.com>
2019-07-19 13:01:46 -0700

    Use babel and make the wrapper optional

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const ts = require('typescript');
+const prettier = require('prettier');
 const visit = require('unist-util-visit');
+const {transformSync} = require('@babel/core');
 
 // TODO: convert to typescript
-
-const compilerOptions = {
-  target: ts.ScriptTarget.ES2017,
-  jsx: ts.JsxEmit.Preserve
-};
 
 function insertAt(array, index, item) {
   return [...array.slice(0, index), item, ...array.slice(index)];
@@ -16,25 +12,40 @@ function isReactComponent(node, tag) {
   return node && node.type === 'jsx' && node.value === tag;
 }
 
-module.exports = ({markdownAST}) => {
+module.exports = (
+  {markdownAST},
+  {wrapper, prettierOptions = {parser: 'babel'}} = {}
+) => {
   visit(markdownAST, 'code', (node, index, parent) => {
     if (/tsx?/.test(node.lang)) {
       const prevNode = parent.children[index - 1];
       const nextNode = parent.children[index + 1];
-      // TODO: make wrapping component checks opt-in
-      // have the user specify the tagname to look for with options
-      if (
-        isReactComponent(prevNode, '<MultiCodeBlock>') &&
-        isReactComponent(nextNode, '</MultiCodeBlock>')
-      ) {
-        const value = ts.transpile(node.value, compilerOptions).trim();
-        if (value) {
+
+      if (wrapper) {
+        const isWrapped =
+          isReactComponent(prevNode, `<${wrapper}>`) &&
+          isReactComponent(nextNode, `</${wrapper}>`);
+        if (!isWrapped) {
+          return;
+        }
+      }
+
+      try {
+        const {code} = transformSync(node.value, {
+          filename: `file.${node.lang}`,
+          retainLines: true,
+          presets: ['@babel/typescript']
+        });
+
+        if (code.trim()) {
           parent.children = insertAt(parent.children, index + 1, {
             type: 'code',
             lang: /x$/.test(node.lang) ? 'jsx' : 'js',
-            value
+            value: prettier.format(code, prettierOptions).trim()
           });
         }
+      } catch (error) {
+        console.error(error.message);
       }
     }
   });
