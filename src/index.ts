f7d25f280c7b673dc3b26883c56a898352d91091
Trevor Blades <tdblades@gmail.com>
2019-08-19 16:31:31 -0700

    Rename 'visitor' to plugin

--- src/index.ts
+++ src/index.ts
@@ -21,47 +21,45 @@ interface Options {
   prettierOptions?: PrettierOptions;
 }
 
-export = function visitor(
+export = function plugin(
   {markdownAST},
   {wrapperComponent, prettierOptions = {parser: 'babel'}}: Options = {}
-): object {
-  visit(
-    markdownAST,
-    'code',
-    (node: Code, index: number, parent: Parent): void => {
-      if (/tsx?/.test(node.lang)) {
-        const prevNode = parent.children[index - 1];
-        const nextNode = parent.children[index + 1];
+): Parent {
+  function visitor(node: Code, index: number, parent: Parent): void {
+    if (/tsx?/.test(node.lang)) {
+      const prevNode = parent.children[index - 1];
+      const nextNode = parent.children[index + 1];
 
-        if (wrapperComponent) {
-          const isWrapped =
-            isReactComponent(prevNode, `<${wrapperComponent}>`) &&
-            isReactComponent(nextNode, `</${wrapperComponent}>`);
-          if (!isWrapped) {
-            return;
-          }
-        }
-
-        try {
-          const {code} = transformSync(node.value, {
-            filename: `file.${node.lang}`,
-            retainLines: true,
-            presets: ['@babel/typescript']
-          });
-
-          if (code.trim()) {
-            parent.children = insertAt(parent.children, index + 1, {
-              type: 'code',
-              lang: /x$/.test(node.lang) ? 'jsx' : 'js',
-              value: format(code, prettierOptions).trim()
-            });
-          }
-        } catch (error) {
-          console.error(error.message);
+      if (wrapperComponent) {
+        const isWrapped =
+          isReactComponent(prevNode, `<${wrapperComponent}>`) &&
+          isReactComponent(nextNode, `</${wrapperComponent}>`);
+        if (!isWrapped) {
+          return;
         }
       }
+
+      try {
+        const {code} = transformSync(node.value, {
+          filename: `file.${node.lang}`,
+          retainLines: true,
+          presets: ['@babel/typescript']
+        });
+
+        if (code.trim()) {
+          parent.children = insertAt(parent.children, index + 1, {
+            type: 'code',
+            lang: /x$/.test(node.lang) ? 'jsx' : 'js',
+            value: format(code, prettierOptions).trim()
+          });
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
     }
-  );
+  }
+
+  visit(markdownAST, 'code', visitor);
 
   return markdownAST;
 };
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,10 @@
 /* eslint-env jest */
-import visitor = require('.');
+import plugin = require('.');
 import {code, root} from 'mdast-builder';
 
 test('transforms TS code blocks', (): void => {
   const ts = code('ts', '(): void => {}');
-  const mdast = visitor({markdownAST: root([ts])});
+  const mdast = plugin({markdownAST: root([ts])});
   expect(mdast).toEqual({
     type: 'root',
     children: [
